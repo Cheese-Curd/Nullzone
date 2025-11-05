b8a577c1a9dc647d1a98150744bd6bc9bca3734f
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2025-11-04 23:55:32 -0500

    Directory Helper

---
+++ movePieces.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+directory = r"./src/main/resources/data/nullzone/structures/nbt/concrete_halls/maze"
+
+for filename in os.listdir(directory):
+    file_path = os.path.join(directory, filename)
+
+    if os.path.isfile(file_path):
+        name_without_ext = os.path.splitext(filename)[0]
+        new_folder_path = os.path.join(directory, name_without_ext)
+        os.makedirs(new_folder_path, exist_ok=True)
+        shutil.move(file_path, os.path.join(new_folder_path, filename))
+
+print("All files moved into their own folders!")
+input("Press enter to dismiss.")
