20cb6b5dc47848c53a83cd806f49cd726ad04e95
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2025-11-06 22:00:09 -0500

    This didn't work, I'll work on it later, not super important :3

--- src/main/java/io/github/cheese_curd/nullzone/blocks/NullzoneBlock.java
+++ src/main/java/io/github/cheese_curd/nullzone/blocks/NullzoneBlock.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.World;
 
 public class NullzoneBlock extends Block
 {
@@ -14,12 +15,20 @@ public class NullzoneBlock extends Block
 		super(settings);
 	}
 
-	@Override
-	public float calcBlockBreakingDelta(BlockState state, PlayerEntity player, BlockView world, BlockPos pos) {
-		if (player.getWorld().getRegistryKey().getRegistry().getNamespace().equals(Nullzone.MOD_ID)
-				&& player.getWorld().getDifficulty() != Difficulty.EASY)
-			return 0f;
+	boolean cannotBreak(PlayerEntity player)
+	{
+		return (player.getWorld().getRegistryKey().getRegistry().getNamespace().equals(Nullzone.MOD_ID)
+			&& player.getWorld().getDifficulty() != Difficulty.EASY);
+	}
 
-		return super.calcBlockBreakingDelta(state, player, world, pos);
+	@Override
+	public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+		if (cannotBreak(player))
+		{
+			world.setBlockState(pos, state, 3);
+			return;
+		}
+
+		super.onBreak(world, pos, state, player);
 	}
 }
