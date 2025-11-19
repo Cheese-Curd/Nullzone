8ab7026b7231eed0dddc233da05fc6b3aab835cf
Eliana <51841108+Cheese-Curd@users.noreply.github.com>
2025-11-18 20:12:19 -0500

    Putting that to detection to use

--- src/main/java/io/github/cheese_curd/nullzone/Nullzone.java
+++ src/main/java/io/github/cheese_curd/nullzone/Nullzone.java
@@ -1,12 +1,13 @@
 package io.github.cheese_curd.nullzone;
 
-import io.github.cheese_curd.nullzone.mixin.PlayerPlacedHolder;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
@@ -16,13 +17,13 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
-import org.quiltmc.qsl.lifecycle.api.event.ServerTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -70,17 +71,23 @@ public class Nullzone implements ModInitializer {
 			.forcedSize(3, 3)
 			.registerPortal();
 
-		AttackBlockCallback.EVENT.register((player, world, hand, pos, dir) ->
+		PlayerBlockBreakEvents.BEFORE.register((world, player, pos, state, ent) ->
 		{
-			if (world.isClient()) return ActionResult.PASS;
+			if (world.isClient()) return true;
+			Identifier worldKey = world.getRegistryKey().getValue();
+			if (!worldKey.getNamespace().equals(MOD_ID)) return true;
+			if (player.isCreative()) return true; // Allow Creative Mode
 
-			BlockState state = world.getBlockState(pos);
-			boolean playerPlaced = ((PlayerPlacedHolder)state.getBlock()).wasPlayerPlaced();
+			ServerWorld serverWorld = (ServerWorld) world;
+			boolean playerPlaced = PlayerPlacedStorage.get(serverWorld).isPlaced(pos);
 
-			if (world.getRegistryKey().getValue().getNamespace().equals(MOD_ID) && !playerPlaced)
-				return ActionResult.FAIL;
+			if (!playerPlaced)
+				if (!worldKey.getPath().equals("stonestills"))
+					if (!state.isOf(ModBlocks.DIRT_STAIN) && !state.isOf(ModBlocks.MOLD_STAIN))
+						return false;
 
-			return ActionResult.PASS;
+			PlayerPlacedStorage.get(serverWorld).clear(pos);
+			return true;
 		});
     }
 }
